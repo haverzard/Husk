6bda229015c9ebf430155b95cf9df58a34aba779
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-07-11 12:27:09 +0700

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+include("../src/Husk.jl")
+
+@test Husk.check(Husk.tokenizer("<Test>World!</Test>Hello<Test/>")) == true
+
+@test Husk.convert_tojson("<Test>World!</Test>Hello<Test/>") == Main.Husk.HtmlJSON("WRAPPER", Dict{String,Any}(), Any[Main.Husk.HtmlJSON("test", Dict{String,Any}(), Any["World!"]), "Hello", Main.Husk.HtmlJSON("test", Dict{String,Any}(), Any[])])
